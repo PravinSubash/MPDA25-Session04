6202dfa7c298d5509af3158163a5683cd63be965
PravinSubash <subashpravin@gmail.com>
2025-04-03 17:07:29 +0200

    Started Working

--- qodo/assignment01.py
+++ qodo/assignment01.py
@@ -1,3 +1,4 @@
+from typing import cast,Any
 import Rhino.Geometry as rg #type:ignore
 import math
 
@@ -6,3 +7,24 @@ x_points = cast(int, x_points)  # type: ignore
 y_points = cast(int, y_points)  # type: ignore
 
 #START CONDING HERE 
+pointList1 = []
+
+# Create points along the X-axis using the slider input "x_points"
+for i in range(x_points):
+    pt = rg.Point3d(i, 0, 0)
+    pointList1.append(pt)
+a = pointList1
+    
+#2. - create second series of points -
+#create a second list of points  by copying the code above, but this time
+#assign the Y coordinate of each point to a value that comes from an 
+#external input which can be a slider in grasshopper called y_points
+ 
+
+pointList2 = []
+# Create points along the Y-axis using the slider input "y_points"
+for i in range(y_points):
+    pt = rg.Point3d(i, 15, 0)
+    pointList2.append(pt)
+b = pointList2
+
